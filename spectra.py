3852b2430e53945ad61ce102ea30812582628d44
Nick <110339671+NickTachmazidis@users.noreply.github.com>
2023-07-17 19:32:30 +0300

    Version 2.1
    Added Type Hints, changed the design of the whole app, added hydra to support .yaml files for settings

---
+++ spectra.py
@@ -0,0 +1,90 @@
+# Core
+from itertools import count
+from typing import Optional, List, Iterator
+# Data/visualisation
+import numpy as np
+from matplotlib.lines import Line2D
+
+class Spectrum():
+    # Initialize count
+    newid: Iterator = count()
+
+    def __init__(self, curve):
+        # ID
+        self.id: int = next(Spectrum.newid)
+        # Data
+        self.curve: Line2D = curve
+        self.x_data: List = self.curve.get_xdata()
+        self.y_data: List = self.curve.get_ydata()
+        # State
+        self.loaded: bool = False
+        self.tristate: int = 1 # -1 tristate, 0 unchecked, 1 checked
+        # Data proccessing
+        self.y_orig: np.array = np.copy(self.y())
+        self.y_smooth: Optional[np.array]
+        self.y_baseline: Optional[np.array]
+        self.y_normalized: Optional[np.array]
+        self.y_normalized_z: Optional[np.array]
+        # Peaks
+        self.peaks: bool = False
+        self.peaks_object: Optional[np.array]
+        self.peaks_color: Optional[str]
+        # Styles
+        self.linewidth: float = self.curve.get_lw()
+        self.linestyle: str = self.curve.get_ls()
+        self.drawstyle: str = self.curve.get_drawstyle() 
+        self.color: str = self.curve.get_color()
+        self.marker: str = self.curve.get_marker()
+        self.marker_size: float = self.curve.get_markersize()
+        self.marker_edge_color: str = self.curve.get_markeredgecolor()
+        self.marker_edge_width: float = self.curve.get_markeredgewidth()
+        self.marker_face_color: str = self.curve.get_markerfacecolor()
+            
+    def label(self) -> str:
+        return self.curve.get_label()
+
+    def x(self) -> List:
+        return self.curve.get_xdata()
+
+    def y(self) -> List:
+        return self.curve.get_ydata()
+
+    def line(self) -> Line2D:
+        return self.curve
+
+    def visible(self) -> None:
+        self.curve.set_visible(True)
+        self.curve.set_color(self.color)
+        self.tristate = 1
+
+    def invisible(self) -> None:
+        self.curve.set_visible(False)
+        self.tristate = 0
+
+    def disabled(self) -> None:
+        self.curve.set_visible(True)
+        self.curve.set_color('grey')
+        self.tristate = -1
+
+    def delete_peaks(self) -> None:
+        self.peaks        = False
+        self.peaks_object = None
+
+    def add_peaks(self, peaks_obj: np.array) -> None:
+        self.peaks = True
+        self.peaks_object = peaks_obj
+
+    def lw(self) -> float:
+        return self.linewidth
+
+    def color(self) -> str:
+        return self.color
+
+    def mrkr_size(self) -> float:
+        return self.marker_size
+
+    def mrkr_face(self) -> str:
+        return self.marker_face_color
+
+    def mrkr_edge(self) -> str:
+        return self.marker_edge_color
