9cb5ee56bdc3b539cc3be90d235be70b3cda4395
NickTachmazidis <ntachmazidis@mayainsights.com>
2024-05-16 22:33:05 +0300

    formatted and annotated canvas.py

--- src/gui/canvas.py
+++ src/gui/canvas.py
@@ -1,34 +1,39 @@
-from PyQt5 import QtCore 
+from PyQt5 import QtCore
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backend_bases import LocationEvent
 from matplotlib.figure import Figure
 import seaborn as sns
 
 # Set style for plotting
-sns.set_style("darkgrid",{
-        'xtick.color':'#999999',
-        'ytick.color':'#999999',
-        'xtick.labelsize': .5,
-        'ytick.labelsize': .5,
-        'font.family':'serif',
-        'axes.grid': True})
+sns.set_style(
+    "darkgrid",
+    {
+        "xtick.color": "#999999",
+        "ytick.color": "#999999",
+        "xtick.labelsize": 0.5,
+        "ytick.labelsize": 0.5,
+        "font.family": "serif",
+        "axes.grid": True,
+    },
+)
 
 
 class Canvas(FigureCanvasQTAgg):
-    def __init__(self, parent=None, width=5, height=5, dpi=160):
+    """Canvas class for the main app."""
+
+    def __init__(self, parent=None, width=5, height=5, dpi=160) -> None:
+        """Initialize the figure and the axes."""
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         super(Canvas, self).__init__(fig)
-        
-        def onframe(event):
-            # make cursor invisible
+
+        def onframe(event: LocationEvent) -> None:
+            """Make the cursor invisible."""
             self.setCursor(QtCore.Qt.BlankCursor)
 
-        fig.canvas.mpl_connect('figure_enter_event',onframe)
-        
+        fig.canvas.mpl_connect("figure_enter_event", onframe)
+
         # Adjust canvas size
-        fig.subplots_adjust(top=0.941,
-                            bottom=0.096,
-                            left=0.043,
-                            right=0.99,
-                            hspace=0.2,
-                            wspace=0.2)
+        fig.subplots_adjust(
+            top=0.941, bottom=0.096, left=0.043, right=0.99, hspace=0.2, wspace=0.2
+        )
